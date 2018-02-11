0fc1209a833fb547b40dfa2abf0adcdcf294a999
Nick Gray <f0rkz@f0rkznet.net>
2018-02-11 01:26:52 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'f0rkz@f0rkznet.net'
 license 'Apache-2.0'
 description 'Installs and configures a Factorio dedicated server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/f0rkz/chef-factorio/issues'
 source_url 'https://github.com/f0rkz/chef-factorio'
